dc7fe67734c71e178a881ab6fe068ed9b7aa2d5c
fransedor <fransedoravelino@gmail.com>
2025-05-13 17:36:17 +0700

    finished tagline and caption

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-			Hello world
-		</div>
+    <div className="container mx-auto p-4 h-screen w-full font-inter flex">
+      <div className="flex items-center h-full w-1/2">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-6xl">Turn notes into quizzes. Turn quizzes into results.</h1>
+          <span className="text-2xl">
+            Go from passive reading to active recall in seconds — Quizzit creates quizzes from any
+            document or webpage.
+          </span>
+        </div>
+      </div>
+      <div className="flex items-center h-full w-1/2">
+        <input type="file" />
+      </div>
+    </div>
   );
 }
